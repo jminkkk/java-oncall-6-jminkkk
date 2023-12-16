500667357c497b7dc6c9a40efd2a1404dd7b753d
‘jminkkk’ <102847513+jminkkk@users.noreply.github.com>
2023-12-16 15:31:38 +0900

    feat: 비상근무순번 예외처리 추가

--- src/main/java/oncall/util/Validator.java
+++ src/main/java/oncall/util/Validator.java
@@ -31,6 +31,11 @@ public class Validator {
 
         validateWorkerDuplication(weekdayWorkersArr);
         validateWorkerDuplication(holidayWorkersArr);
+        validateWeekdayToHoliday(weekdayWorkersArr, holidayWorkersArr);
+        Arrays.stream(weekdayWorkersArr).forEach(Validator::validateNickname);
+        Arrays.stream(holidayWorkersArr).forEach(Validator::validateNickname);
+        validateWorkerCount(weekdayWorkersArr.length);
+        validateWorkerCount(holidayWorkersArr.length);
     }
     private static void validateWorkerDuplication(String[] workers) {
         int workerCount = (int) Arrays.stream(workers)
@@ -38,7 +43,31 @@ public class Validator {
                 .count();
 
         if (workerCount != workers.length) {
-            throw new IllegalArgumentException("[ERROR] 유효하지 않은 입력 값입니다. 다시 입력해 주세요.");
+            throw new IllegalArgumentException("[ERROR] 유효하지 않은 입력 값입니다. 다시 입력해 주세요.(입력받은 이름 중 중복이 존재하는 경우)");
+        }
+    }
+
+    private static void validateWeekdayToHoliday(String[] weekdayWorkersArr, String[] holidayWorkersArr) {
+        if (weekdayWorkersArr.length != holidayWorkersArr.length) {
+            throw new IllegalArgumentException("[ERROR] 유효하지 않은 입력 값입니다. 다시 입력해 주세요.(평일 순번, 휴일 순번 입력 개수가 다른 경우)");
+        }
+
+        if (Arrays.stream(weekdayWorkersArr)
+                .anyMatch(worker -> Arrays.stream(holidayWorkersArr)
+                        .anyMatch(worker::equals))) {
+            throw new IllegalArgumentException("[ERROR] 유효하지 않은 입력 값입니다. 다시 입력해 주세요.(평일 순번, 휴일 순번에 각각 1회 편성되지 않은 경우)");
+        }
+    }
+
+    private static void validateNickname(String nickname) {
+        if (nickname.length() > 5) {
+            throw new IllegalArgumentException("[ERROR] 유효하지 않은 입력 값입니다. 다시 입력해 주세요.(닉네임 길이가 5자를 초과하는 경우)");
+        }
+    }
+
+    private static void validateWorkerCount(int workerCount) {
+        if (workerCount < 5 || workerCount > 35) {
+            throw new IllegalArgumentException("[ERROR] 유효하지 않은 입력 값입니다. 다시 입력해 주세요.(근무자 수가 5명 미만 또는 35명 초과인 경우)");
         }
     }
 }

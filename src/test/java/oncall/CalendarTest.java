07f7263edd403c713046c37629860b92044d4152
‘jminkkk’ <102847513+jminkkk@users.noreply.github.com>
2023-12-16 17:33:21 +0900

    test: 법정 공휴일 테스트

---
+++ src/test/java/oncall/CalendarTest.java
@@ -0,0 +1,15 @@
+package oncall;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import oncall.domain.Calendar;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+
+class CalendarTest {
+    @ParameterizedTest
+    @CsvSource({"JANUARY, 1", "MARCH, 1", "MAY, 5", "JUNE, 6", "AUGUST, 15", "OCTOBER, 3", "OCTOBER, 9", "DECEMBER, 25"})
+    void holiday(Calendar calendar, int holiday) {
+        assertThat(calendar.getHolidays().contains(holiday)).isTrue();
+    }
+}

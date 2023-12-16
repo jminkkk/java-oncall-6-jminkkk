bf706cbcb30b2513b28acf5558aa3287e0eb27cd
‘jminkkk’ <102847513+jminkkk@users.noreply.github.com>
2023-12-16 15:55:59 +0900

    feat: 비상근무표를 나타내는 EmergencySchedule

---
+++ src/main/java/oncall/domain/EmergencySchedule.java
@@ -0,0 +1,35 @@
+package oncall.domain;
+
+import java.util.List;
+
+public class EmergencySchedule {
+    private final Calendar month;
+    private final DaysOfTheWeek firstYoil;
+    private final List<String> workers;
+
+    public EmergencySchedule(Calendar month, DaysOfTheWeek firstYoil, List<String> workers) {
+        this.month = month;
+        this.firstYoil = firstYoil;
+        this.workers = workers;
+    }
+
+    @Override
+    public String toString() {
+        StringBuilder sb = new StringBuilder();
+        int firstYoilIndex = firstYoil.getIndex();
+        for (int i = 0; i < month.getLastDay(); i++) {
+            String yoil = DaysOfTheWeek.of(firstYoilIndex % 7).toString();
+
+            if (month.getHolidays().contains(i + 1)) {
+                yoil += "(휴일)";
+            }
+
+            sb.append(month.getMonth()).append("월 ")
+                    .append(i + 1).append("일 ")
+                    .append(yoil + " ")
+                    .append(workers.get(i)).append("\n");
+            firstYoilIndex++;
+        }
+        return sb.toString();
+    }
+}
